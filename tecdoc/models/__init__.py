1672dfd10d5ed237a9d833a2f7710cf45b4e5ccf
maho1 <sinisa-2@hotmail.com>
2014-01-07 17:24:44 +0100

    Revert "test"
    This reverts commit 11ab1f89ff5cc451d00cc6ab800105f74de1c477.

--- tecdoc/models/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from tecdoc.models.base import *
-from tecdoc.models.car import *
-from tecdoc.models.category import *
-from tecdoc.models.common import *
-from tecdoc.models.criteria import *
-from tecdoc.models.graphics import *
-from tecdoc.models.part import *
-from tecdoc.models.supplier import *
